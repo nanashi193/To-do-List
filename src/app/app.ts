87dc22800003bc5dd0d1a00f3c32185d3573ea4a
nanashi193 <dqhungg8357@gmail.com>
2025-09-27 16:01:28 +0700

    feat(add): new function add

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
-  protected readonly title = signal('angular-todo');
+  title = 'angular-todo';
+  todos = ["hoc Angular", "lam To-do List project", "Ngu?"];
+  newTodo = ''; //bien cho input
+
+  add(){
+    const t = this.newTodo.trim();
+    if(!t) return;  //chan input rong
+    this.todos.unshift(t); //them vao dau danh sach
+    this.newTodo = '';
+  }
+
 }
